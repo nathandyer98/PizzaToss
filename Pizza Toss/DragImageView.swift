b9c9d146aa0299f840b17d4f4731fa6bc06a2d5b
nd17aak <nd17aak@herts.ac.uk>
2020-10-08 19:10:57 +0100

    Final commit

---
+++ Pizza Toss/DragImageView.swift
@@ -0,0 +1,30 @@
+//
+//  DragImageView.swift
+//  Pizza Toss
+//
+//  Created by nd17aak on 13/01/2020.
+//  Copyright © 2020 nd17aak. All rights reserved.
+//
+
+import UIKit
+
+class DragImageView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pizza Toss/FinishedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FinishedViewController.swift
+//  Pizza Toss
+//
+//  Created by nd17aak on 13/01/2020.
+//  Copyright © 2020 nd17aak. All rights reserved.
+//
+
+import UIKit
+
+class FinishedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
